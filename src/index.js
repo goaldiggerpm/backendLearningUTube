dd44e7960d0f8724ad62fe67a416bde74211eb72
goaldiggerpm <47710070+goaldiggerpm@users.noreply.github.com>
2024-01-03 00:40:04 +0530

    utils added Dev - Prince

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@
 import dotenv from 'dotenv'
 
 import connectDB from './db/index.js'
+import { app } from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`😈 Server is running at port: ${process.env.PORT}`)
+        })
 
-
-
-
-
-
+        app.on("error", () => {
+            console.error("Error after connection", error)
+            throw error
+        })
+    })
+    .catch((err) => {
+        console.log('MONGODB connection failed !!!', err)
+    })
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,20 @@
+class ApiResponse extends Error {
+    constructor(
+        statusCode, data, message = "Success"
+    ) {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = true
+        this.errors = statusCode < 400
+
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiResponse }
