7fa54f2c89b4c031ae0e2bff7556623084996d11
goaldiggerpm <47710070+goaldiggerpm@users.noreply.github.com>
2024-01-02 22:30:34 +0530

    connect to mongo Atlas Dev - Prince

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({ path: './env' })
+
+import dotenv from 'dotenv'
+
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", () => {
+//             console.error("Error after connection", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.port}`)
+//         })
+//     } catch (error) {
+//         console.error("Error we get", error)
+//         throw error
+//     }
+// })()
